8e56339c4d7ee730c268a852e53e737e4ef226a7
Art Rosnovsky <art.rosnovsky@auth0.com>
2022-10-04 09:16:14 -0700

    I made a mess

--- src/components/Dashboard/UrlList.tsx
+++
@@ -1,9 +0,0 @@
-import Url from './Url'
-
-export default function UrlList({ urls }: any) {
-  return (
-    <div className="my-10">
-      <Url url={urls} />
-    </div>
-  )
-}
--- src/components/Dashboard/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Sidebar } from './Sidebar';
-import Url from './Url';
-import UrlList from './UrlList';
-
-export { Sidebar, Url, UrlList };
